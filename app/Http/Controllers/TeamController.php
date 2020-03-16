5119cf258f5569ff829f6c1801729dea18a04ec3
Samikshya Regmi <sraj@Samikshyas-MacBook-Air.local>
2020-03-16 14:18:32 +0545

    team order in team page in sdlkfjsdl

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -32,10 +32,11 @@ class TeamController extends Controller
 //        $teamList = Team::whereYear('date', '=', $year)->get();
         $usersUnique = Team::select(DB::raw('count(id) as `data`'), DB::raw("DATE_FORMAT(date, '%Y') new_date"),  DB::raw('YEAR(date) year, MONTH(date) year'))
         ->groupby('date','year')
+            ->orderBy('date','desc')
         ->get();
 
 
-        $teamList = $usersUnique->unique('new_date')->orderBy('new_date','desc');
+        $teamList = $usersUnique->unique('new_date');
 
 //        $teamList = $this->team->where('is_active','1')->orderBy('date','desc')->get()->groupBy(function($val) {
 //            return Carbon::parse($val->date)->format('Y');
