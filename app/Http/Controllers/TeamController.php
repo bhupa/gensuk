f51b1376812cf9e0567a6c5694490d4f97e057af
Samikshya Regmi <sraj@Samikshyas-MacBook-Air.local>
2020-03-16 17:12:36 +0545

    eror in where qie sdsd fsfdssddsd

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -32,7 +32,6 @@ class TeamController extends Controller
 //        $teamList = Team::whereYear('date', '=', $year)->get();
         $usersUnique = Team::select(DB::raw('count(id) as `data`'), DB::raw("DATE_FORMAT(date, '%Y') new_date"),  DB::raw('YEAR(date) year, MONTH(date) year'))
         ->groupby('date','year')
-//            ->where('is-active','1')
             ->orderBy('date','desc')
         ->get();
 
